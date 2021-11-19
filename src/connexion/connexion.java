c602ec20bdde3810dc7afec10011eee03dae361b
Skyrolight <skyrolight@gmail.com>
2021-11-19 23:24:23 +0100

    Creation connection

--- src/connexion/connexion.java
+++ src/connexion/connexion.java
@@ -1,1 +1,53 @@
 package connexion;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class connexion {
+
+    // init database constants
+    private static final String DATABASE_URL = "jdbc:mysql://localhost:3306/todo_list";
+    private static final String USERNAME = "Damien";
+    private static final String PASSWORD = "5K15A9ZcmVU6rAlj";
+
+    // init connection object
+    private Connection connection;
+    // init properties object
+    private Properties properties;
+
+    // create properties
+    private Properties getProperties() {
+        if (properties == null) {
+            properties = new Properties();
+            properties.setProperty("user", USERNAME);
+            properties.setProperty("password", PASSWORD);
+        }
+        return properties;
+    }
+
+    // connect database
+    public Connection creeConnection() {
+        if (connection == null) {
+            try {
+                connection = DriverManager.getConnection(DATABASE_URL, getProperties());
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return connection;
+    }
+
+    // disconnect database
+    public void disconnect() {
+        if (connection != null) {
+            try {
+                connection.close();
+                connection = null;
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
--- src/main/main.java
+++ src/main/main.java
@@ -1,4 +1,26 @@
 package main;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import connexion.connexion;
+
 public class main {
-    
+    public static void main(String[] args) {
+        connexion maConnexion = new connexion();
+        try {
+            PreparedStatement statement = maConnexion.creeConnection().prepareStatement("SELECT * FROM compte");
+            ResultSet res = statement.executeQuery();
+            while (res.next()) {
+                int id = res.getInt("idCompte");
+                String pseudo = res.getString("pseudo");
+                String mdp = res.getString("mot_de_passe");
+                String admail = res.getString("ad_mail");
+
+                System.out.println("ID: " + id + " pseudo: " + pseudo + " mdp: " + mdp + " Adresse mail: "+admail);
+            }
+        } catch (Exception e) {
+            //TODO: handle exception
+        }
+    }
 }
